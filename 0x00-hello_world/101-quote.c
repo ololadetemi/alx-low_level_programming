46d93b7ab1193a65c3703c61b8ec842195a31b59
ololadetemi <ololadetoluwalase@gmail.com>
2022-10-17 14:36:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,10 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful 
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	int l;
+	l=
+		sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+	write(2,
+			"and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
 	return (1);
 }
