7d62f3549360bee2a8c1416e3776ff4221240b17
ololadetemi <ololadetoluwalase@gmail.com>
2022-10-17 11:18:55 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
  * main - prints the exact string
- *
- * Return: Always 0
+ * Return: Always 1
  */
-int main(oid)
+int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful" 
+			\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
