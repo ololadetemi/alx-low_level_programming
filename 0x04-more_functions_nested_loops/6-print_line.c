4effd228222b80441e34aa36d62b73c0123e4946
ololadetemi <ololadetoluwalase@gmail.com>
2022-10-21 15:21:59 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (x = 0; x <= n; x++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
