3665c099fc22cc0494f234bddbcf7567d56fd02d
ololadetemi <ololadetoluwalase@gmail.com>
2022-10-21 15:27:30 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,13 +5,18 @@
  */
 void print_line(int n)
 {
-	int x;
-
 	if (n <= 0)
-		_putchar('\n');
-	for (x = 0; x <= n; x++)
 	{
-		_putchar('_');
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
