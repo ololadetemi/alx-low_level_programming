fa247a5ffcb3d059749fbafc3ce10d2b55327a73
ololadetemi <ololadetoluwalase@gmail.com>
2022-10-21 21:05:53 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,20 +6,26 @@
  */
 int main(void)
 {
-	int n = 1;
+	int i;
 
-	while (n <= 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz");
-		else if (n % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
+		{
 			printf("Fizz");
-		else if (n % 5 == 0)
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
 			printf("Buzz");
-		else
-			printf("%d", n);
-		if (n != 100)
-			printf(" ");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+			{
+				printf("%d", i);
+			}
 	}
 	printf("\n");
 	return (0);
