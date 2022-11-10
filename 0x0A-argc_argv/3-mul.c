ed1cdad3b3a65258edc2854d5e2bba7a1fdcbfbe
ololadetemi <ololadetoluwalase@gmail.com>
2022-11-09 16:12:56 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -> function to print its name
+ * @argc: argc param
+ * @argv: array of command listed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+		result = num1 * num2;
+
+		printf("%d\n", result);
+		return (0);
+}				
