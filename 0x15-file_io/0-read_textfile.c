62252bc7fc635e1e623955c682cea3c224b88835
ololadetemi <ololadetoluwalase@gmail.com>
2022-12-05 13:47:46 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,6 @@
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
- *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
